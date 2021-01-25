c3911b193eb746af2ad684ee4422276f6cea1606
Zulqurnain Akhter <iamzulqurnain@gmail.com>
2021-01-25 06:25:14 +0500

    Starting khoojee project 🕵️
    Developing project for foot printing collecting open source information available freely over the internet.

---
+++ Khoojee/urls.py
@@ -0,0 +1,27 @@
+"""Khoojee URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+
+from app.view.home_page import home
+from app.view.search_domain_info import home as domain_info
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home_page"),
+    path('domain_info/', domain_info, name="domain_info"),
+]
