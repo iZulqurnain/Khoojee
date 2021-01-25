7e50ea15af86af49a540b16dc9463ea16210d251
Zulqurnain Akhter <iamzulqurnain@gmail.com>
2021-01-25 06:34:19 +0500

    Adding static and celery information
    Celery and static information was missing in  project

--- Khoojee/settings.py
+++ Khoojee/settings.py
@@ -128,4 +128,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# REDIS related settings
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
+CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+
+# for Heroku
+CELERY_BROKER_URL = os.environ.get('REDISCLOUD_URL', 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0')
+CELERY_RESULT_BACKEND = os.environ.get('REDISCLOUD_URL', 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0')
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+
+if 'ON_HEROKU' in os.environ:
+    django_heroku.settings(locals())
